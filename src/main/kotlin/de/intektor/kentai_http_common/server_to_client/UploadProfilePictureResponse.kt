13d78bc7cdd72c126e47bff51eba2215ecdb4207
Tim <geilomat4@gmail.com>
2018-04-15 21:23:20 +0200

    15.04.2018

---
+++ src/main/kotlin/de/intektor/kentai_http_common/server_to_client/UploadProfilePictureResponse.kt
@@ -0,0 +1,13 @@
+package de.intektor.kentai_http_common.server_to_client
+
+/**
+ * @author Intektor
+ */
+class UploadProfilePictureResponse(val type: Type) {
+
+    enum class Type {
+        SUCCESS,
+        FAILED_UNKNOWN_USER,
+        FAILED_VERIFY_SIGNATURE
+    }
+}
